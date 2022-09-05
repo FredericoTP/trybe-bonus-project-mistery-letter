d8c8e1858a05fdf933907b6106269921bc86c2fc
Frederico Tavares Pessoa <fredericotp@hotmail.com>
2022-09-05 17:47:52 -0300

    Requisito 16

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
 const inputCartaTexto = document.getElementById('carta-texto');
 const buttonCriarCarta = document.getElementById('criar-carta');
 const pCartaGerada = document.getElementById('carta-gerada');
+const tagSpan = document.getElementsByTagName('span');
+const styleGroup = ['newspaper', 'magazine1', 'magazine2'];
+const sizeGroup = ['medium', 'big', 'reallybig'];
+const rotationGroup = ['rotateleft', 'rotateright'];
+const inclinationGroup = ['skewleft', 'skewright'];
+
+function randomClass(element) {
+  const random = Math.floor(Math.random() * element.length);
+  return random;
+}
+
+function classSpanElements() {
+  const conjunto = [];
+  conjunto.push(styleGroup[randomClass(styleGroup)]);
+  conjunto.push(sizeGroup[randomClass(sizeGroup)]);
+  conjunto.push(rotationGroup[randomClass(rotationGroup)]);
+  conjunto.push(inclinationGroup[randomClass(inclinationGroup)]);
+  return conjunto;
+}
+
+function removeVirgula() {
+  const classes = classSpanElements().toString().replace(/,/g, ' ');
+  return classes;
+}
 
 function createLetter() {
   const content = inputCartaTexto.value;
@@ -12,7 +36,7 @@ function createLetter() {
   }
 
   for (let index = 0; index < splitContent.length; index += 1) {
-    pCartaGerada.innerHTML += `<span>${splitContent[index]}<span>`;
+    pCartaGerada.innerHTML += `<span class=${removeVirgula()}>${splitContent[index]}<span>`;
   }
 
   if (content.length == 0 || spaceVerification.length == 0) {
