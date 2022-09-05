4533b2f16c08ddfc5c2567bf9016033886ac7127
Frederico Tavares Pessoa <fredericotp@hotmail.com>
2022-09-05 16:15:53 -0300

    Requisito 5

--- script.js
+++ script.js
@@ -5,13 +5,18 @@ const pCartaGerada = document.getElementById('carta-gerada');
 function createLetter() {
   const content = inputCartaTexto.value;
   const splitContent = content.split(' ');
+  const spaceVerification = content.trim();
 
   if (pCartaGerada.innerHTML.length > 0) {
     pCartaGerada.innerHTML = '';
   }
 
   for (let index = 0; index < splitContent.length; index += 1) {
-    pCartaGerada.innerHTML += `<span>${splitContent[index]}<span>`
+    pCartaGerada.innerHTML += `<span>${splitContent[index]}<span>`;
+  }
+
+  if (content.length == 0 || spaceVerification.length == 0) {
+    pCartaGerada.innerText = 'Por favor, digite o conteúdo da carta.';
   }
 }
 
